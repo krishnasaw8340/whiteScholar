c1aca3ea5b5715b036e5b351d5241428adccb02a
Amit Kumar Sharma <91947037+amit-2k1@users.noreply.github.com>
2022-07-15 00:53:36 +0530

    notes ui

--- src/Component/ResumeCard.jsx
+++ src/Component/ResumeCard.jsx
@@ -1,7 +1,16 @@
 import { DownloadIcon } from "@chakra-ui/icons";
 import { Box, Flex, IconButton, Image, Text } from "@chakra-ui/react";
 
-const ResumeCard = ({ item }) => {
+const getColor = () => {
+  let color = "#";
+  for (let i = 0; i < 3; i++)
+    color += ("0" + Math.floor(((1 + Math.random()) * Math.pow(16, 2)) / 2).toString(16)).slice(-2);
+  return color;
+}
+
+const ResumeCard = ({ item, ...rest }) => {
+  const borderColor = getColor()
+
   const onDownload = item => {
     const link = document.createElement("a");
     link.download = item.name + ".docx";
@@ -10,27 +19,27 @@ const ResumeCard = ({ item }) => {
   };
 
   return (
-    <Box border="4px #2077ea solid" borderRadius={4}>
+    <Box border={`4px ${borderColor} solid`}  borderRadius={4} {...rest}>
       <Image
         src={`/resources/resumes/${item.imagePath}`}
         transition="transform 300ms ease-in-out"
         _hover={{
           transform: "scale(120%, 120%)",
-          border: "2px #2077ea solid",
+          border: `2px ${borderColor} solid`,
         }}
         _focus={{
           transform: "scale(200%, 200%)",
-          border: "2px #2077ea solid",
+          border: `2px ${borderColor} solid`,
         }}
         alt={item.name + " resume"}
       />
       <Flex p={2} justify="space-between" align="center">
-        <Text fontSize="lg" color="#2077ea" fontWeight="bold">
+        <Text fontSize="lg" color="" fontWeight="bold">
           {item.name}
         </Text>
         <IconButton
           icon={<DownloadIcon color="white" />}
-          bg="#2077ea"
+          bg=""
           justifySelf="flex-end"
           _hover={{
             bg: "blue.600",
